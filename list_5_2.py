10d36435469ff031d7802baa5274f4130f7d3cf9
Fadhil Ikhsan <f.ikhsanta@gmail.com>
2020-05-28 19:52:42 +0700

    Update list_5_2.py

--- list_5_2.py
+++ list_5_2.py
@@ -17,8 +17,8 @@ def ftp_upload(ftp_server, username, password, file_name):
     print ("Login to FTP server: user=%s" %username)
     ftp.login(username, password)
     ext = os.path.splitext(file_name)[1]
-    if ext in (".txt", ".htm", ".html"):
-        ftp.storlines("STOR " + file_name, open(file_name))
+    if ext in ():
+        ftp.storlines("STOR " + file_name, open(file_name.encode))
     else:
         ftp.storbinary("STOR " + file_name, open(file_name, "rb"), 1024)
     print ("Uploaded file: %s" %file_name)
@@ -31,6 +31,8 @@ if __name__ == '__main__':
     parser.add_argument('--username', action="store", dest="username", default=getpass.getuser())
     given_args = parser.parse_args() 
     ftp_server, file_name, username = given_args.ftp_server, given_args.file_name, given_args.username
-    password = getpass.getpass(prompt="Enter you FTP password: ") 
+    password = getpass.getpass(prompt="Enter you FTP password: ")
+    username="fadhil"
     ftp_upload(ftp_server, username, password, file_name)
+
 
